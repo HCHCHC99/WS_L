4ed9e14b263466c820605b285e3389cf1655e03a
HCHCHC99 <1785067760@qq.com>
2026-08-09 00:42:58 +0800

    目前可动，comm_mode = 10开环方向和闭环方向反了

--- ws_L_v.1.0.2/HC32F460_DDL_Rev3.3.0/projects/ev_hc32f460_lqfp100_v2/ws/cur_loop.c
+++ ws_L_v.1.0.2/HC32F460_DDL_Rev3.3.0/projects/ev_hc32f460_lqfp100_v2/ws/cur_loop.c
@@ -24,7 +24,7 @@
 #include "rtt_log.h"
 
 #define CURLOOP_WIN_SIZE     5u    /* 5-tap sliding average at 25kHz (~100us) */
-#define CURLOOP_DT_FIRST_US  40u    /* assumed 25kHz period for the very first call */
+#define CURLOOP_DT_FIRST_US  20u    /* assumed 25kHz period for the very first call */
 #define CURLOOP_DUTY_RATE    1.0f  /* max duty change per control cycle (%) */
 
 /* Keil Watch: current setpoint (mA) */
